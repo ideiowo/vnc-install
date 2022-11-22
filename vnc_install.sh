6c9b7f724d54a244c0dd3b6cf833458c427ec579
鍾鎬澤 <107016864+ideiowo@users.noreply.github.com>
2022-11-22 23:05:24 +0800

    vnc_install.sh
    install

---
+++ vnc_install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install vino
+mkdir -p ~/.config/autostart
+cp /usr/share/applications/vino-server.desktop ~/.config/autostart
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password $(echo -n jetson'|base64)
+sudo reboot
