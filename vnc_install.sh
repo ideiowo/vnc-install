03a82cf75f0e7899c4ea794e7c3a9dded243d5d3
鍾鎬澤 <107016864+ideiowo@users.noreply.github.com>
2022-11-22 23:51:55 +0800

    Update vnc_install.sh

--- vnc_install.sh
+++ vnc_install.sh
@@ -1,11 +1,4 @@
 sudo apt update
-sudo apt install vino
-mkdir -p ~/.config/autostart
-cp /usr/share/applications/vino-server.desktop ~/.config/autostart
-gsettings set org.gnome.Vino prompt-enabled false
-gsettings set org.gnome.Vino require-encryption false
-gsettings set org.gnome.Vino authentication-methods "['vnc']"
-gsettings set org.gnome.Vino vnc-password $(echo -n jetson'|base64)
 pip install pycryptodome
 pip install cryto
 sudo pip install ecdsa
@@ -13,4 +6,11 @@ echo 'export PATH=/home/ideiowo/.local/bin:$PATH' >>~/.bashrc~
 source ~/.bashrc
 export PATH=$PATH:/home/ideiowo/.local/bin
 source $HOME/.bashrc
+sudo apt install vino
+mkdir -p ~/.config/autostart
+cp /usr/share/applications/vino-server.desktop ~/.config/autostart
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password $(echo -n jetson'|base64)
 sudo reboot
